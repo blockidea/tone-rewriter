95b133ca6c57b1972c465fc8cebc3efc41c16b28
Maggie H. <40013730+blockidea@users.noreply.github.com>
2026-04-27 18:21:13 -0600

    Update page.js

--- app/success/page.js
+++ app/success/page.js
@@ -11,7 +11,12 @@ export default function Success() {
     <div style={{ padding: '40px', textAlign: 'center' }}>
       <h1>Payment Successful 🎉</h1>
       <p>You now have unlimited access.</p >
-      <a href=" ">Go back</a >
+      <a
+  href="https://tone-rewriter-eight.vercel.app/"
+  style={{ fontSize: '20px', color: 'blue' }}
+>
+  Go back to app
+</a>
     </div>
   );
 }
